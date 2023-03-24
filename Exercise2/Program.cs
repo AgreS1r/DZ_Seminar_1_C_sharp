da8a021f546332b145172ecb5e41ef15cd8ad57f
danil <faceitplay2004@mail.ru>
2023-03-24 21:58:14 +0300

    Задача 4

---
+++ Exercise2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter numberA : ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter numberB : ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter numberC : ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max){
+    max = numberB;
+}
+if (numberC > max){
+    max = numberC;
+}
+Console.WriteLine($"max = {max}");
