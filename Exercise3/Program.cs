296c65428cd0b052f8de7674da1b45e0f87ee3eb
danil <faceitplay2004@mail.ru>
2023-03-24 22:05:45 +0300

    Задача 6

---
+++ Exercise3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter number : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0){
+    Console.WriteLine($"number {number} - чётное!");
+}
+else Console.WriteLine($"number {number} - нечётное!");
