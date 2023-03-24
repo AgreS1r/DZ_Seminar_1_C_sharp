4a5658c51227f460e59721fe39338a930cf05eca
danil <faceitplay2004@mail.ru>
2023-03-24 21:36:17 +0300

    Задача 2

---
+++ Exercise1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter numberA : ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter numberB : ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB) Console.WriteLine("numberA - max, numberB - min");
+else Console.WriteLine("numberB - max, numberA - min");
